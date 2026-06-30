3dd43a3d145551f9a6a19d17efdea11dfa8496bd
fcolucasvieira <fclucasvieira@gmail.com>
2026-06-30 10:43:15 -0300

    chore: initialize Spring Boot project

---
+++ src/test/java/br/com/jardelcell/inventory/InventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jardelcell.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
