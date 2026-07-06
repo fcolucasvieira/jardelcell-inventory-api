f19b1291a27c648f08001320f833d3e6673d9a77
fcolucasvieira <fclucasvieira@gmail.com>
2026-07-06 11:04:57 -0300

    feat: map user entity

---
+++ src/main/java/br/com/jardelcell/inventory/user/Role.java
@@ -0,0 +1,6 @@
+package br.com.jardelcell.inventory.user;
+
+public enum Role {
+    ADMIN,
+    EMPLOYEE
+}
---
+++ src/main/java/br/com/jardelcell/inventory/user/User.java
@@ -0,0 +1,51 @@
+package br.com.jardelcell.inventory.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "full_name", nullable = false, length = 255)
+    private String fullName;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "update_at")
+    private OffsetDateTime updatedAt;
+
+    public User(String fullName, String email, String password, Role role) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.active = true;
+    }
+}
