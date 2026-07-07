527ba27a0b3878c987a6a4f9affc3760b1c57678
fcolucasvieira <fclucasvieira@gmail.com>
2026-07-07 00:29:23 -0300

    feat: configure JWT properties

--- src/main/java/br/com/jardelcell/inventory/InventoryApiApplication.java
+++ src/main/java/br/com/jardelcell/inventory/InventoryApiApplication.java
@@ -2,8 +2,10 @@ package br.com.jardelcell.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class InventoryApiApplication {
 
 	public static void main(String[] args) {
