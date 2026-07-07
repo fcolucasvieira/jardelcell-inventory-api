9d39271cffda36783a2e8db27ad0db59298207a7
fcolucasvieira <fclucasvieira@gmail.com>
2026-07-07 00:15:01 -0300

    feat: initialize authentication services

--- src/main/java/br/com/jardelcell/inventory/auth/AuthenticationService.java
+++ src/main/java/br/com/jardelcell/inventory/auth/AuthenticationService.java
@@ -1,4 +1,13 @@
 package br.com.jardelcell.inventory.auth;
 
+import br.com.jardelcell.inventory.security.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 }
