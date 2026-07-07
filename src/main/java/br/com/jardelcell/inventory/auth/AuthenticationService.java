17adf904847b40eb640fa3be95a71bd15aafc097
fcolucasvieira <fclucasvieira@gmail.com>
2026-07-07 01:14:19 -0300

    feat: implement authentication service

--- src/main/java/br/com/jardelcell/inventory/auth/AuthenticationService.java
+++ src/main/java/br/com/jardelcell/inventory/auth/AuthenticationService.java
@@ -1,8 +1,13 @@
 package br.com.jardelcell.inventory.auth;
 
+import br.com.jardelcell.inventory.auth.dto.LoginRequest;
+import br.com.jardelcell.inventory.auth.dto.LoginResponse;
 import br.com.jardelcell.inventory.security.TokenService;
+import br.com.jardelcell.inventory.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +15,19 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
+
+    public LoginResponse login(LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+
+        User authenticatedUser = (User) authentication.getPrincipal();
+
+        String token = tokenService.generateToken(authenticatedUser);
+
+        return new LoginResponse(token);
+    }
 }
