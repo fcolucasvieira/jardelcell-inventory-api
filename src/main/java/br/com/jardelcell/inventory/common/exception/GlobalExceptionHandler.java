affc083e71cfe834bd9f96632be01b2cc6701e19
fcolucasvieira <fclucasvieira@gmail.com>
2026-07-10 14:47:07 -0300

    feat(exception): add global exception handler

---
+++ src/main/java/br/com/jardelcell/inventory/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package br.com.jardelcell.inventory.common.exception;
+
+import br.com.jardelcell.inventory.common.dto.ApiErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    public ResponseEntity<ApiErrorResponse> handleProductAlreadyExists(
+            ProductAlreadyExistsException ex,
+            HttpServletRequest request
+    ) {
+
+        ApiErrorResponse response =
+                new ApiErrorResponse(
+                        Instant.now(),
+                        HttpStatus.CONFLICT.value(),
+                        HttpStatus.CONFLICT.getReasonPhrase(),
+                        ex.getMessage(),
+                        request.getRequestURI()
+                );
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(response);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleResourceNotFound(
+            ProductAlreadyExistsException ex,
+            HttpServletRequest request
+    ) {
+
+        ApiErrorResponse response =
+                new ApiErrorResponse(
+                        Instant.now(),
+                        HttpStatus.NOT_FOUND.value(),
+                        HttpStatus.NOT_FOUND.getReasonPhrase(),
+                        ex.getMessage(),
+                        request.getRequestURI()
+                );
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+}
