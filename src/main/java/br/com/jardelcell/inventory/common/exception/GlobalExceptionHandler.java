46d6a6383536b08eaf68054efa72894e4edae27a
fcolucasvieira <fclucasvieira@gmail.com>
2026-07-11 14:11:12 -0300

    refactor(exception): centralize business exception handling

--- src/main/java/br/com/jardelcell/inventory/common/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/jardelcell/inventory/common/exception/GlobalExceptionHandler.java
@@ -11,27 +11,17 @@ import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(ProductAlreadyExistsException.class)
-    public ResponseEntity<ApiErrorResponse> handleProductAlreadyExists(
-            ProductAlreadyExistsException ex,
-            HttpServletRequest request
-    ) {
-        return buildErrorResponse(ex, HttpStatus.CONFLICT, request);
-    }
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiErrorResponse> handleResourceNotFound(
-            ResourceNotFoundException ex,
-            HttpServletRequest request
-    ) {
-        return buildErrorResponse(ex, HttpStatus.NOT_FOUND, request);
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ApiErrorResponse> handleBusinessException(BusinessException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request);
     }
 
     private ResponseEntity<ApiErrorResponse> buildErrorResponse(
-            Exception ex,
-            HttpStatus status,
+            BusinessException ex,
             HttpServletRequest request
     ) {
+        HttpStatus status = ex.getStatus();
+
         ApiErrorResponse response =
                 new ApiErrorResponse(
                         Instant.now(),
