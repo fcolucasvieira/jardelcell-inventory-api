32e2ec95ab1e790a43127523b3d70309311a430f
fcolucasvieira <lucas.vieira@alu.ufc.br>
2026-07-17 17:20:06 -0300

    feat(product): implement unreserve product use case

---
+++ src/main/java/br/com/jardelcell/inventory/product/usecase/UnreserveProductUseCase.java
@@ -0,0 +1,36 @@
+package br.com.jardelcell.inventory.product.usecase;
+
+import br.com.jardelcell.inventory.common.exception.ResourceNotFoundException;
+import br.com.jardelcell.inventory.inventory.InventoryMovementService;
+import br.com.jardelcell.inventory.product.Product;
+import br.com.jardelcell.inventory.product.ProductMapper;
+import br.com.jardelcell.inventory.product.ProductRepository;
+import br.com.jardelcell.inventory.product.dto.ProductResponse;
+import br.com.jardelcell.inventory.product.dto.UnreserveProductRequest;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UnreserveProductUseCase {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+    private final InventoryMovementService inventoryMovementService;
+
+    @Transactional
+    public ProductResponse execute(UUID productId, UnreserveProductRequest request) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Product not found with Id: " + productId
+                ));
+
+        product.markAsInStock();
+
+        inventoryMovementService.registerUnreserve(product, request.observation());
+
+        return productMapper.toResponse(product);
+    }
+}
