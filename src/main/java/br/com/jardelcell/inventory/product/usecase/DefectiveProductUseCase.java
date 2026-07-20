bde9aaefca0c845f2657169abb39bdd62177b125
fcolucasvieira <lucas.vieira@alu.ufc.br>
2026-07-20 10:08:09 -0300

    feat(product): implement defective product use case

---
+++ src/main/java/br/com/jardelcell/inventory/product/usecase/DefectiveProductUseCase.java
@@ -0,0 +1,36 @@
+package br.com.jardelcell.inventory.product.usecase;
+
+import br.com.jardelcell.inventory.common.exception.ResourceNotFoundException;
+import br.com.jardelcell.inventory.inventory.InventoryMovementService;
+import br.com.jardelcell.inventory.product.Product;
+import br.com.jardelcell.inventory.product.ProductMapper;
+import br.com.jardelcell.inventory.product.ProductRepository;
+import br.com.jardelcell.inventory.product.dto.DefectiveProductRequest;
+import br.com.jardelcell.inventory.product.dto.ProductResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DefectiveProductUseCase {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+    private final InventoryMovementService inventoryMovementService;
+
+    @Transactional
+    public ProductResponse execute(UUID productId, DefectiveProductRequest request) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Product not found with Id: " + productId
+                ));
+
+        product.markAsDefective();
+
+        inventoryMovementService.registerDefective(product, request.observation());
+
+        return productMapper.toResponse(product);
+    }
+}
