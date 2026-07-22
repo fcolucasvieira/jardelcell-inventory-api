45026952773361c56ed5b936d7251b714bf28b8a
fcolucasvieira <lucas.vieira@alu.ufc.br>
2026-07-22 13:51:17 -0300

    feat(validation): handle bean validation exception

--- src/main/java/br/com/jardelcell/inventory/common/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/jardelcell/inventory/common/exception/GlobalExceptionHandler.java
@@ -1,16 +1,40 @@
 package br.com.jardelcell.inventory.common.exception;
 
 import br.com.jardelcell.inventory.common.dto.ApiErrorResponse;
+import br.com.jardelcell.inventory.common.dto.FieldValidationError;
+import br.com.jardelcell.inventory.common.dto.ValidationErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        List<FieldValidationError> errors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fieldError -> new FieldValidationError(
+                        fieldError.getField(),
+                        fieldError.getDefaultMessage()
+                        )
+                )
+                .toList();
+
+        ValidationErrorResponse response = new ValidationErrorResponse(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                errors
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ApiErrorResponse> handleBusinessException(BusinessException ex, HttpServletRequest request) {
         return buildErrorResponse(ex, request);
